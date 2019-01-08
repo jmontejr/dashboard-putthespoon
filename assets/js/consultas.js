ddefde6a2d6bdc00d691fc896a063514251da25a
jmontejr <josecmontejr@gmail.com>
2019-01-08 02:33:18 -0200

    Call API Generic GET Method

---
+++ assets/js/consultas.js
@@ -0,0 +1,44 @@
+Consultas = {
+
+    urlDefault: "https://putthespoon-api.herokuapp.com/api/1",
+
+    getUrl: function (url, arg1 = null, arg2 = null, arg3 = null){
+        var url_complete = Consultas.urlDefault + url;
+        if (arg1 != null) {
+            url_complete = url_complete + '/' + arg1;
+            if (arg2 != null) {
+                url_complete = url_complete + '/' + arg2;
+                if (arg3 != null) {
+                    url_complete = url_complete + '/' + arg3;
+                    return url_complete;
+                }
+                return url_complete;
+            }
+            return url_complete;
+        }
+        return url_complete;
+    },
+    
+    requestGet: function (url, arg1=null, arg2=null, arg3=null) {
+
+        $.ajax({
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true'
+            },
+            type: "GET",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            crossDomain: true,
+            url: Consultas.getUrl(url, arg1, arg2, arg3),
+            data: {},
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (e) {
+                console.log("Error: ", e);
+            }
+        });
+    }
+
+};
